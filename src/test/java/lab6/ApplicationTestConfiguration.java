02b8c355be4e95f26763947f24c3b938d88f996e
Vasilii Tolkachev <vasilii_tolkachev@epam.com>
2018-06-10 18:53:09 +0300

    lab6 add db configuration

---
+++ src/test/java/lab6/ApplicationTestConfiguration.java
@@ -0,0 +1,28 @@
+package lab6;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+
+@Configuration
+@Import({lab5.configuration.ApplicationTestConfiguration.class})
+@ComponentScan("lab6")
+public class ApplicationTestConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("db-schema.sql")
+                .build();
+    }
+}
