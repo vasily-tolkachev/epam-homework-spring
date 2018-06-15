76200100db8b0137c04a7106296f21471871f67f
Vasilii Tolkachev <vasilii_tolkachev@epam.com>
2018-06-15 16:20:37 +0300

    lab7 JPA

--- src/main/java/lab7/dao/jpa/AbstractJpaDao.java
+++ src/main/java/lab7/dao/jpa/AbstractJpaDao.java
@@ -1,19 +1,20 @@
 package lab7.dao.jpa;
 
+import lab6.dao.CountryDao;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
-public class AbstractJpaDao {
+import static lombok.AccessLevel.PRIVATE;
 
+@FieldDefaults(level = PRIVATE)
+public abstract class AbstractJpaDao implements CountryDao, JpaDao {
+
+    @Getter
+    @Setter(onMethod = @__(@PersistenceUnit))
     protected EntityManagerFactory emf;
-
-    public AbstractJpaDao() {
-        super();
-    }
-
-    @PersistenceUnit
-    public void setEntityManagerFactory(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
 
 }
